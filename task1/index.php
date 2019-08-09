51076f6c2e30530ad2d19ff35fe2762897025a34
cheefs <cheefst@outlook.com>
2019-08-09 22:32:05 +0700

    возможность просмтра pdf, ini, txt, и изображений

--- task1/index.php
+++ task1/index.php
@@ -1,7 +1,22 @@
 <?php
+define( 'ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
 
-$dir = $_POST['name']?? 'c://';
-$recursiveDir = new DirectoryIterator($dir.'/');
+$currentPath = $_POST['path']?? 'c://'; // сохранение пути, если выбрали файл
+$dir = $_POST['name'] && is_dir($_POST['name'])? $_POST['name'] : $currentPath;
+$recursiveDir = new RecursiveDirectoryIterator($dir.'/');
+
+$selectedFile = null;
+
+if (is_file($_POST['name'])) {
+
+    $selectedFile = $_POST['name'];
+    $file = explode(DIRECTORY_SEPARATOR, $selectedFile);
+    $file = $file[count($file) - 1];
+
+
+    copy($selectedFile,ROOT_PATH.'/src/'.$file);
+    $selectedFile = 'http://algoritms/src/'.$file;
+}
 
 ?>
 
@@ -24,6 +39,7 @@ $recursiveDir = new DirectoryIterator($dir.'/');
         ?>
             <?php if ( $fileName == '.'): ?>
                 <h3><?= $file->getPath() ?> </h3>
+                <input hidden value="<?= $file->getPath() ?>" name="path">
             <?php elseif( $fileName == '..'): ?>
                 <div class="header">
                     <input class="input" type="submit" value="<?= $path ?>" name="name"><br>
@@ -32,12 +48,15 @@ $recursiveDir = new DirectoryIterator($dir.'/');
             <?php elseif( trim($file->getRealPath()) !== ''): ?>
             <div class="list">
                 <label class="<?= $isDir? 'show' : 'hide' ?>" for="<?= $path ?>">&#8226;</label>
-                <input id="<?= $path ?>" class="<?= $isDir? 'dir': 'file' ?>" type="<?= $isDir? 'submit' : 'button' ?>" value="<?= $path ?>" name="name">
+                <input class="<?= $isDir? 'dir': 'file' ?>" type="submit" value="<?= $path ?>" name="name">
             </div>
-
             <?php endif; ?>
         <?php endforeach; ?>
     </form>
+
+    <div class="preview">
+        <object class="obj" data="<?= $selectedFile ?>"></object>
+    </div>
 </body>
 </html>
 
